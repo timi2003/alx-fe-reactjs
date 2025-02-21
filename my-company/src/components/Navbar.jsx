bf6e9ac6435f12afdec642fa7775bb9854778cb8
Oluwafemi <kolawolefemi2003@gmail.com>
2025-02-21 11:03:56 +0100

    my company

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+    <nav style={{ 
+      padding: '10px', 
+      backgroundColor: '#f0f0f0', 
+      display: 'flex', 
+      justifyContent: 'space-around' 
+    }}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>

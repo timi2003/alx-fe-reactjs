51e2d139f1d7678f96b53a217df118fa59029490
Oluwafemi <kolawolefemi2003@gmail.com>
2025-03-18 15:04:05 +0100

    tailwind

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -4,8 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // 'media' for system preference, 'class' for manual
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

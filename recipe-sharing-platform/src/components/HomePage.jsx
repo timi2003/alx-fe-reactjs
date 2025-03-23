70efd0c847fd5b968ecbe576c3a8a0d6a757a52c
Oluwafemi <kolawolefemi2003@gmail.com>
2025-03-23 22:04:04 +0100

    recipe-sharing

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,11 +11,14 @@ const HomePage = () => {
     return (
         <div className="border-indigo-400 p-8 rounded-lg shadow-lg">
             <h1 className="text-xl text-indigo-800">Recipe Collection</h1>
-            <div className="shadow-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {recipes.map(recipe => (
-                    <div key={recipe.id}>
-                        <h2>{recipe.title}</h2>
-                        <p>{recipe.summary}</p>
+                    <div 
+                        key={recipe.id} 
+                        className="p-4 border rounded-lg hover:bg-indigo-100 transition duration-300"
+                    >
+                        <h2 className="text-lg font-semibold">{recipe.title}</h2>
+                        <p className="text-gray-700">{recipe.summary}</p>
                     </div>
                 ))}
             </div>
